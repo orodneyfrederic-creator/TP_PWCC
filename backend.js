36a8b4f2c041049e3de4da8c53d64169004933ae
Rody Odims <Kiwi>
2026-02-27 02:12:13 +0100

    Lancement de mon serveur modifié

--- backend.js
+++ backend.js
@@ -1,18 +1,34 @@
 // On appel le module natif http qui prendra en charge ce que le front reçoit et ce qu'il envoit au backend
-const http = require('http');
+    const http = require('http');
 
  // il est utilisé afin d'analyser les requêtes plus facilement
- const url = require('url'); 
+    const url = require('url'); 
 
 
 //  Crétion de mon server Javascript
-const server = http.createServer ((res,req) => {
+    const server = http.createServer ((req ,res) => {
 
     // Analyse de l'URL qui arrive
-    const parsedUrl = url.parsed(req.url, true)
+    const parsedUrl = url.parse(req.url, true);
     
 
     // extraire le chemin d'accès
-    const parthName = (parsedUrl.parthName)
+    const pathname = parsedUrl.pathname ;
     
+
+    //extraire les paramètres de la requête
+    const queryData = parsedUrl.query;
+
+
+    console.log("Quelqu'un appelle le chemin :", pathname);
+    console.log("Avec ces paramètres :", queryData);
+
+    // Pour ne pas que mon navigateur tourne dans le vide 
+
+    res.end ("Bien reçu !");
+});
+
+// Ecoute du serveur sur le port 3000
+server.listen(3000, () => {
+    console.log("Le serveur est lancé sur le port 3000 de http://localhost:3000");
 })
