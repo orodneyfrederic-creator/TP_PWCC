3bfb53c6f5ced367c07425e6c6f5da9f864d4f40
marie <melvonanga@gmail.com>
2026-02-28 23:23:26 +0100

    J'ai fais les favoris

--- backend.js
+++ backend.js
@@ -1,12 +1,15 @@
 // On appel le module natif http qui prendra en charge ce que le front reçoit et ce qu'il envoit au backend
-    const http = require('http');
+    const { on } = require('cluster');
+const http = require('http');
 
  // il est utilisé afin d'analyser les requêtes plus facilement
     const url = require('url'); 
-
+    
+    // pour stocker les favories
+    let favorites = [];
 
 //  Crétion de mon server Javascript
-    const server = http.createServer ((req ,res) => {
+    const server = http.createServer ((req ,res) => {   
 
     // Analyse de l'URL qui arrive
     const parsedUrl = url.parse(req.url, true);
@@ -28,16 +31,67 @@
     if (pathname === "/api/test" && req.method === "GET") // verifie l'addresse et recupère des infos
     
 { 
-    res.writeHead ( 200,{  "content-Type": "application/json" }); // code succès données en Json
+    res.writeHead ( 200,{  "Content-Type":"application/json" }); // code succès données en Json
 
+   
     res.end(JSON.stringify ({ message: " API fonctionne"})); // envoi la données et ferme la connexion
+    return
+    }
 
-    return;
-}
+    // ici tout sur les favories
 
+    // obtenir des favoris
+
+    if (pathname === "/api/favorites") 
+        {
+            if (req.method === "GET")
+                {
+                    res.writeHead (200,{"Content-Type":"application/json"} )
+                    res.end (JSON.stringify(favorites))
+                    return
+                }
+    
+
+    // envoie les données des favoris dans le corps
+    
+            if (req.method === "POST")
+                {
+                    let body = "";
+                    req.on("data",chunk =>
+                        { 
+                            body += chunk.toString();   // envoie un Post et les données arrive en chunks   chunks veut dir morceau
+                        });
+        
+                    req.on("end",() => 
+                        {
+                            const newFavorite = JSON.parse(body);   //la toutes les donnés sont recus
+                            favorites.push(newFavorite);
+
+                            res.writeHead (201,{ "Content-Type":"application/json"})
+                            res.end(JSON.stringify({message:"ajouté au favoris"}))
+                            
+                        })
+                }return;
+            }
+
+        // suprimer les favoris
+if (pathname.startsWith("/api/favorites/") && req.method === "DELETE") 
+        {
+            const id =pathname.split("/") [3];
+            favorites = favorites.filter( fav => fav.id !=id );
+    
+            console.log("Tentative de suppression de l'ID :", id);
+
+            res.writeHead (200, {"Content-Type":"application/json"})
+            res.end (JSON.stringify({message:"Supprimé"}))
+            return;
+        } 
+        
     // Pour ne pas que mon navigateur tourne dans le vide 
     
-    res.end ("Bien reçu !");
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Route non trouvée" }));
+    
 });
     
     // Ecoute du serveur sur le port 3000
